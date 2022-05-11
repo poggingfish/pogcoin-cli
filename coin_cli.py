f768e3f1e2ccb8e6c1da8167873c7b6a3a7b9348
Pogging <dylstructor@gmail.com>
2022-05-11 15:09:23 -0500

    Very slow but it works ig

--- coin_cli.py
+++ coin_cli.py
@@ -1,13 +1,28 @@
-endpoint = "http://localhost:8080" 
+endpoint = "https://coin.pogging.fish" 
 import requests
-def create_tx(addr1, addr2, amount, password):    
+import time
+def create_tx(addr1, addr2, amount, password):
+    global endpoint    
     request = requests.post(endpoint + "/tx", f"{addr1} {addr2} {amount} {password}")
+    if request.text == "_CLIENT_TRYAGAIN":
+        time.sleep(0.659)
+        create_tx(addr1, addr2, amount, password)
+        return 0
     print(request.text)
 def create_wallet():
+    global endpoint
     print("Creating wallet...")
-    request = requests.post(endpoint + "/create-wallet")
+    request = requests.get(endpoint + "/create-wallet")
     return request.text.split(" ")[0], request.text.split(" ")[1]
- 
+def get_balance(addr):
+    global endpoint
+    text = "NO_ARGS_PASSED"
+    while text == "NO_ARGS_PASSED":
+        request = requests.post(endpoint + "/get-balance", f"{addr}")   
+        text = request.text
+        time.sleep(0.659)
+    balance = request.text
+    return balance
 def arg_parser():
     import sys
     if len(sys.argv) == 1:
@@ -44,10 +59,20 @@ if __name__ == "__main__":
             command = command.split(" ")
             base = command[0]
             if base == "send":
-                create_tx(name, command[1], float(command[2]), password)
+                if float(command[2]) < 3:
+                    print("Sorry. Minimum amount is 3 PogCoin.")
+                    continue
+                usr_input = input("Are you sure you want to send "+ command[2] + " pogcoin to " + command[1] + "? (y/n) ")
+                if usr_input == "y":
+                    print("Creating transaction...")
+                    create_tx(name, command[1], float(command[2]), password)
+                if usr_input == "n":
+                    print("Cancelled.")
             elif base == "exit":
                 break
             elif base == "addr":
                 print(f"Your address is {name}")
                 print("Share this with others to receive PogCoin!")
+            elif base == "balance":
+                print(f"Your balance is {get_balance(name)} PogCoin")
 print("Exiting...")
